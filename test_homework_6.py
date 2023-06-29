6b4eae81e2f6c1839c9f68739ae8985117603800
Darya Andronova <dbukhareva@yandex.ru>
2023-06-29 14:13:44 +0300

    Поправлена ошибка в функции test_dark_theme_by_time_and_user_choice

--- test_homework_6.py
+++ test_homework_6.py
@@ -8,7 +8,7 @@ def test_dark_theme_by_time():
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
-    is_dark_theme = time(hour=6) <= current_time >= time(hour=22)
+    is_dark_theme = current_time.hour >= 22 or current_time.hour <= 6
     print(is_dark_theme)
     assert is_dark_theme is True
 
@@ -21,18 +21,14 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = False - Темная тема выключена
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
     """
-    current_time = time(hour=16)
-    dark_theme_enabled_by_user = True
+    current_time = time(hour=4)
+    dark_theme_enabled_by_user = None
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
-
     if dark_theme_enabled_by_user is not None:
         is_dark_theme = dark_theme_enabled_by_user
-    elif time(hour=6) <= current_time >= time(hour=22):
-        is_dark_theme = True
     else:
-        is_dark_theme = False
-
+        is_dark_theme = current_time.hour >= 22 or current_time.hour <= 6
     assert is_dark_theme is True
 
 
